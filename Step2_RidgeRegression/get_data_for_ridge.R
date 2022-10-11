d9b2512b9b96023f94a6580911603529f272b642
askeller <arielleskeller@gmail.com>
2022-10-11 14:28:31 -0400

    get rid of duplicate subject

--- Step2_RidgeRegression/get_data_for_ridge.R
+++ Step2_RidgeRegression/get_data_for_ridge.R
@@ -61,5 +61,5 @@ abcd.data.train <- abcd.data.traintest[abcd.data.traintest$matched_group==1,]
 abcd.data.test <- abcd.data.traintest[abcd.data.traintest$matched_group==2,]
 abcd.data.for.ridge<-abcd.data.traintest[,c("subjectkey","thompson_PC1","thompson_PC2","thompson_PC3","matched_group","interview_age","sex","meanFD","abcd_site","rel_family_id")]
 abcd.data.for.ridge.complete<-abcd.data.for.ridge[complete.cases(abcd.data.for.ridge),]
-
+abcd.data.for.ridge.complete<-abcd.data.for.ridge.complete %>% distinct()
 write.csv(abcd.data.for.ridge.complete,"/Users/askeller/Documents/Kellernet_PrelimAnalysis/data_for_ridge.csv")
