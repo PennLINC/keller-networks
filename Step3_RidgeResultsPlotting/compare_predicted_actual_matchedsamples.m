92351d08d15b20f4c3f6e491d456c74a5e154000
askeller <arielleskeller@gmail.com>
2023-04-12 09:58:54 -0400

    sensitivity analyses and hex plots

--- Step3_RidgeResultsPlotting/compare_predicted_actual_matchedsamples.m
+++ Step3_RidgeResultsPlotting/compare_predicted_actual_matchedsamples.m
@@ -49,7 +49,7 @@ actual_PC3_samp2 = actual_PC3(data_for_ridge(:,4)==2);
 %% Load in the results for bootstrapping and null distributions
 
 % ~~~~~ NULL ~~~~~~ %
-folder_name='/Users/askeller/Documents/Kellernet_PrelimAnalysis/RidgeResults/results_matchedsamples_null_081822/';
+folder_name='/Users/askeller/Documents/Kellernet_PrelimAnalysis/RidgeResults/results_matchedsamples_null_081822';
 file_name_acc_PC1_null_samp1='thompson_PC1_acc_null_testA.npy';
 file_name_acc_PC1_null_samp2='thompson_PC1_acc_null_testB.npy';
 file_name_acc_PC2_null_samp1='thompson_PC2_acc_null_testA.npy';
@@ -207,6 +207,82 @@ axis([-0.1 0.1 0 30])
 box off
 
 
+%% Replot the scatterplots as hexplots
+
+figure()
+
+subplot(2,3,1)
+hexscatter(actual_PC1_samp1,pred_PC1_samp1,'res',40)
+hold on
+linearFit = polyfit(actual_PC1_samp1,pred_PC1_samp1,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=3;
+colormap("hot")
+xlabel("Actual Cognitive Performance",'FontSize',12)
+ylabel("Predicted Cognitive Performance",'FontSize',12)
+axis([-4 4 -4 4])
+
+subplot(2,3,4)
+hexscatter(actual_PC1_samp2,pred_PC1_samp2,'res',40)
+hold on
+linearFit = polyfit(actual_PC1_samp2,pred_PC1_samp2,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=3;
+colormap("hot")
+xlabel("Actual Cognitive Performance",'FontSize',12)
+ylabel("Predicted Cognitive Performance",'FontSize',12)
+axis([-4 4 -4 4])
+
+subplot(2,3,2)
+hexscatter(actual_PC2_samp1,pred_PC2_samp1,'res',40)
+hold on
+linearFit = polyfit(actual_PC2_samp1,pred_PC2_samp1,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=3;
+colormap("hot")
+xlabel("Actual Cognitive Performance",'FontSize',12)
+ylabel("Predicted Cognitive Performance",'FontSize',12)
+axis([-4 4 -4 4])
+
+subplot(2,3,5)
+hexscatter(actual_PC2_samp2,pred_PC2_samp2,'res',40)
+hold on
+linearFit = polyfit(actual_PC2_samp2,pred_PC2_samp2,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=3;
+colormap("hot")
+xlabel("Actual Cognitive Performance",'FontSize',12)
+ylabel("Predicted Cognitive Performance",'FontSize',12)
+axis([-4 4 -4 4])
+
+subplot(2,3,3)
+hexscatter(actual_PC3_samp1,pred_PC3_samp1,'res',40)
+hold on
+linearFit = polyfit(actual_PC3_samp1,pred_PC3_samp1,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=3;
+colormap("hot")
+xlabel("Actual Cognitive Performance",'FontSize',12)
+ylabel("Predicted Cognitive Performance",'FontSize',12)
+axis([-4 4 -4 4])
+
+subplot(2,3,6)
+hexscatter(actual_PC3_samp2,pred_PC3_samp2,'res',40)
+hold on
+linearFit = polyfit(actual_PC3_samp2,pred_PC3_samp2,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=3;
+colormap("hot")
+xlabel("Actual Cognitive Performance",'FontSize',12)
+ylabel("Predicted Cognitive Performance",'FontSize',12)
+axis([-4 4 -4 4])
+
 
 %% Make histograms for the bootstrapped distributions:
 
@@ -277,3 +353,200 @@ axis([-0.05 0.05 0 30])
 
 
 
+%% Sensitivity analysis: Separate Samples PCA
+
+% Load in prediction results
+folder_name='/Users/askeller/Documents/Kellernet_PrelimAnalysis/RevisionAnalyses_PFNsCog/SeparateSamplesCognitionPCA/results/all_network';
+file_name_pred1_samp1='SepSampPC1_prediction_testA.npy';
+file_name_pred2_samp1='SepSampPC2_prediction_testA.npy';
+file_name_pred3_samp1='SepSampPC3_prediction_testA.npy';
+file_name_pred1_samp2='SepSampPC1_prediction_testB.npy';
+file_name_pred2_samp2='SepSampPC2_prediction_testB.npy';
+file_name_pred3_samp2='SepSampPC3_prediction_testB.npy';
+pred_PC1_samp1 = readNPY([folder_name '/' file_name_pred1_samp1]);
+pred_PC2_samp1 = readNPY([folder_name '/' file_name_pred2_samp1]);
+pred_PC3_samp1 = readNPY([folder_name '/' file_name_pred3_samp1]);
+pred_PC1_samp2 = readNPY([folder_name '/' file_name_pred1_samp2]);
+pred_PC2_samp2 = readNPY([folder_name '/' file_name_pred2_samp2]);
+pred_PC3_samp2 = readNPY([folder_name '/' file_name_pred3_samp2]);
+
+% put predictions on same scale as actual
+pred_PC1_samp1 = zscore(pred_PC1_samp1);
+pred_PC2_samp1 = zscore(pred_PC2_samp1);
+pred_PC3_samp1 = zscore(pred_PC3_samp1);
+pred_PC1_samp2 = zscore(pred_PC1_samp2);
+pred_PC2_samp2 = zscore(pred_PC2_samp2);
+pred_PC3_samp2 = zscore(pred_PC3_samp2);
+
+% fourth column has the matched_group
+data_for_ridge=xlsread('/Users/askeller/Documents/Kellernet_PrelimAnalysis/RevisionAnalyses_PFNsCog/SeparateSamplesCognitionPCA/data_for_ridge_SepSampPCA.xls');
+
+% Load in actual cognitive scores
+actual_PC1 = data_for_ridge(:,1);
+actual_PC2 = data_for_ridge(:,2);
+actual_PC3 = data_for_ridge(:,3);
+
+% split data out by matched group
+actual_PC1_samp1 = actual_PC1(data_for_ridge(:,4)==1);
+actual_PC1_samp2 = actual_PC1(data_for_ridge(:,4)==2);
+actual_PC2_samp1 = actual_PC2(data_for_ridge(:,4)==1);
+actual_PC2_samp2 = actual_PC2(data_for_ridge(:,4)==2);
+actual_PC3_samp1 = actual_PC3(data_for_ridge(:,4)==1);
+actual_PC3_samp2 = actual_PC3(data_for_ridge(:,4)==2);
+
+
+% load colors
+color_fill = gray(10);
+color_fill_1 = color_fill(1,:);
+color_fill_2 = color_fill(7,:);
+
+figure()
+scatter(actual_PC1_samp2,pred_PC1_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
+[r,p,ci,stats]=corrcoef(actual_PC1_samp2,pred_PC1_samp2)
+hold on
+scatter(actual_PC1_samp1,pred_PC1_samp1,75,color_fill_1,'Marker','.')
+[r,p,ci,stats]=corrcoef(actual_PC1_samp1,pred_PC1_samp1)
+hold on
+linearFit = polyfit(actual_PC1_samp1,pred_PC1_samp1,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=2;
+hold on
+linearFit = polyfit(actual_PC1_samp2,pred_PC1_samp2,1);
+hline = refline(linearFit);
+hline.Color=color_fill_2;
+hline.LineWidth=2;
+xlabel("Actual Cognitive Performance",'FontSize',16)
+ylabel("Predicted Cognitive Performance",'FontSize',16)
+axis([-4 4 -4 4])
+
+
+
+figure()
+scatter(actual_PC2_samp2,pred_PC2_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
+[r,p,ci,stats]=corrcoef(actual_PC2_samp2,pred_PC2_samp2)
+hold on
+scatter(actual_PC2_samp1,pred_PC2_samp1,75,color_fill_1,'Marker','.')
+[r,p,ci,stats]=corrcoef(actual_PC2_samp1,pred_PC2_samp1)
+hold on
+linearFit = polyfit(actual_PC2_samp1,pred_PC2_samp1,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=2;
+hold on
+linearFit = polyfit(actual_PC2_samp2,pred_PC2_samp2,1);
+hline = refline(linearFit);
+hline.Color=color_fill_2;
+hline.LineWidth=2;
+xlabel("Actual Cognitive Performance",'FontSize',16)
+ylabel("Predicted Cognitive Performance",'FontSize',16)
+axis([-4 4 -4 4])
+
+
+figure()
+scatter(actual_PC3_samp2,pred_PC3_samp2,15,color_fill_2,'Marker','^','MarkerFaceColor',color_fill_2)
+[r,p,ci,stats]=corrcoef(actual_PC3_samp2,pred_PC3_samp2)
+hold on
+scatter(actual_PC3_samp1,pred_PC3_samp1,75,color_fill_1,'Marker','.')
+[r,p,ci,stats]=corrcoef(actual_PC3_samp1,pred_PC3_samp1)
+hold on
+linearFit = polyfit(actual_PC3_samp1,pred_PC3_samp1,1);
+hline = refline(linearFit);
+hline.Color=color_fill_1;
+hline.LineWidth=2;
+hold on
+linearFit = polyfit(actual_PC3_samp2,pred_PC3_samp2,1);
+hline = refline(linearFit);
+hline.Color=color_fill_2;
+hline.LineWidth=2;
+xlabel("Actual Cognitive Performance",'FontSize',16)
+ylabel("Predicted Cognitive Performance",'FontSize',16)
+axis([-4 4 -4 4])
+
+
+
+%% SEP SAMP NULL
+
+
+% fourth column has the matched_group
+data_for_ridge=xlsread('/Users/askeller/Documents/Kellernet_PrelimAnalysis/RevisionAnalyses_PFNsCog/SeparateSamplesCognitionPCA/data_for_ridge_SepSampPCA.xls');
+
+
+% load colors
+color_fill = gray(10);
+color_fill_1 = color_fill(1,:);
+color_fill_2 = color_fill(7,:);
+
+
+% ~~~~~ NULL ~~~~~~ %
+folder_name='/Users/askeller/Documents/Kellernet_PrelimAnalysis/RevisionAnalyses_PFNsCog/SeparateSamplesCognitionPCA/results/all_network_null/';
+file_name_acc_PC1_null_samp1='SepSampPC1_acc_null_testA_group1.npy';
+file_name_acc_PC1_null_samp2='SepSampPC1_acc_null_testB_group1.npy';
+file_name_acc_PC2_null_samp1='SepSampPC2_acc_null_testA_group1.npy';
+file_name_acc_PC2_null_samp2='SepSampPC2_acc_null_testB_group1.npy';
+file_name_acc_PC3_null_samp1='SepSampPC3_acc_null_testA_group1.npy';
+file_name_acc_PC3_null_samp2='SepSampPC3_acc_null_testB_group1.npy';
+acc_PC1_null_samp1 = readNPY([folder_name '/' file_name_acc_PC1_null_samp1]);
+acc_PC1_null_samp2 = readNPY([folder_name '/' file_name_acc_PC1_null_samp2]);
+acc_PC2_null_samp1 = readNPY([folder_name '/' file_name_acc_PC2_null_samp1]);
+acc_PC2_null_samp2 = readNPY([folder_name '/' file_name_acc_PC2_null_samp2]);
+acc_PC3_null_samp1 = readNPY([folder_name '/' file_name_acc_PC3_null_samp1]);
+acc_PC3_null_samp2 = readNPY([folder_name '/' file_name_acc_PC3_null_samp2]);
+
+
+figure()
+subplot(2,1,1)
+hist(acc_PC1_null_samp1)
+h=findobj(gca,'Type','patch')
+h.FaceColor = color_fill_1;
+h.EdgeColor = color_fill_1;
+set(gca,'ytick',[])
+hold on
+axis([-0.1 0.1 0 30])
+box off
+subplot(2,1,2)
+hist(acc_PC1_null_samp2)
+h=findobj(gca,'Type','patch')
+h.FaceColor = color_fill_2;
+h.EdgeColor = color_fill_2;
+set(gca,'ytick',[])
+axis([-0.1 0.1 0 30])
+box off
+
+
+figure()
+subplot(2,1,1)
+hist(acc_PC2_null_samp1)
+h=findobj(gca,'Type','patch')
+h.FaceColor = color_fill_1;
+h.EdgeColor = color_fill_1;
+set(gca,'ytick',[])
+axis([-0.1 0.1 0 30])
+subplot(2,1,2)
+hist(acc_PC2_null_samp2)
+h=findobj(gca,'Type','patch')
+h.FaceColor = color_fill_2;
+h.EdgeColor = color_fill_2;
+set(gca,'ytick',[])
+axis([-0.1 0.1 0 30])
+box off
+
+
+
+figure()
+subplot(2,1,1)
+hist(acc_PC3_null_samp1)
+h=findobj(gca,'Type','patch')
+h.FaceColor = color_fill_1;
+h.EdgeColor = color_fill_1;
+axis([-0.1 0.1 0 30])
+set(gca,'ytick',[])
+box off
+subplot(2,1,2)
+hist(acc_PC3_null_samp2)
+h=findobj(gca,'Type','patch')
+h.FaceColor = color_fill_2;
+h.EdgeColor = color_fill_2;
+set(gca,'ytick',[])
+axis([-0.1 0.1 0 30])
+box off
